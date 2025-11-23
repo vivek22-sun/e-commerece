6896f2a00d99e7b041d8a52683f55706048e5af3
Vik947 <vivekvik50@gmail.com>
2025-11-23 19:18:53 +0530

    Push updates

--- React_E-Commerce/src/components/Footer.jsx
+++ React_E-Commerce/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">Copyright © 2025 All rights reserved by{" "}
-              <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Vivek</a>
+              <a  href="https://github.com/vivek22-sun/e-commerece" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Vivek</a>
             </p>
             <a className="text-dark fs-4" href="https://github.com/vivek22-sun/e-commerece" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
